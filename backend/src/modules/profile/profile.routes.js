792a48bd6e7260e3639836a1748ba9b13e6d8cd8
Rahyan-Akil <86472807+rahyanakil@users.noreply.github.com>
2026-06-12 17:33:57 +0600

    Delete backend directory

--- backend/src/modules/profile/profile.routes.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const router = require('express').Router()
-const protect = require('../../middleware/auth.middleware')
-const { getProfile, upsertProfile } = require('./profile.controller')
-
-router.get('/', getProfile)
-router.put('/admin', protect, upsertProfile)
-
-module.exports = router
--- backend/src/modules/upload/upload.routes.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const router = require('express').Router()
-const protect = require('../../middleware/auth.middleware')
-const { getSignedUploadUrl, deleteImage } = require('./upload.controller')
-
-router.post('/sign', protect, getSignedUploadUrl)
-router.delete('/delete', protect, deleteImage)
-
-module.exports = router
